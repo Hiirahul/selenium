b0603f1506d7e3205a872c53c354b49868acea3d
ASUS <ASUS@LAPTOP-ESLD2BDJ.lan>
2022-05-04 09:46:19 +0530

    First Commit...

---
+++ SeleniumPractice/src/javaSelenium/configuration.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package javaSelenium;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class configuration{
+
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+	
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+	//	driver.quit();
+	}
+
+}
